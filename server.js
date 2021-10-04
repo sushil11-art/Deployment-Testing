3271bd7e423cb3078660fff2475b01c37abfb4ac
Sushil <phuyalsushil1189@@example.com>
2021-10-04 15:04:20 +0545

    finally deploying to heroku

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.send('Home page of app');
+})
+
 app.get("/demo",(req,res)=>{
     // let message="This is demo api for deployment";
     res.send('Hello World!');
